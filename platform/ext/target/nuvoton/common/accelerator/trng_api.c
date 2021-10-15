58c92e556a69bd6e4d14fa49c5486a7689738929
WSChang0 <wschang0@nuvoton.com>
2021-10-15 20:07:05 +0800

    Uptimize entropy generation
    Change-Id: I36ddffcea32918652cbcea657f251d504febc74a

--- platform/ext/target/nuvoton/common/accelerator/trng_api.c
+++ platform/ext/target/nuvoton/common/accelerator/trng_api.c
@@ -30,8 +30,6 @@ int mbedtls_hardware_poll( void *data,
     int32_t i;
     int32_t timeout = 0x1000;
     uint32_t u32Reg;
-    int32_t err = 0;
-
 
     ARG_UNUSED(data);
 
@@ -45,36 +43,39 @@ int mbedtls_hardware_poll( void *data,
         return -1;
 
     /* Generate the seed by TRNG */
-
-    CLK->AHBCLK |= CLK_AHBCLK_CRPTCKEN_Msk;
-    CLK->APBCLK1 |= CLK_APBCLK1_TRNGCKEN_Msk;
-    CLK->APBCLK0 |= CLK_APBCLK0_RTCCKEN_Msk;
-
-    RTC->LXTCTL |= (RTC_LXTCTL_C32KSEL_Msk | RTC_LXTCTL_LIRC32KEN_Msk); //To use LIRC32K
-
-    TRNG->ACT |= TRNG_ACT_ACT_Msk;
-    /* Waiting for ready */
-    i = 0;
-    while((TRNG->CTL & TRNG_CTL_READY_Msk) == 0)
+    if((TRNG->CTL & TRNG_CTL_READY_Msk) == 0)
     {
-        if(i++ > timeout)
-        {
-            /* TRNG ready timeout */
-            return -1;
-        }
-    }
+        /* Initial only when it is not ready */
+        CLK->AHBCLK |= CLK_AHBCLK_CRPTCKEN_Msk;
+        CLK->APBCLK1 |= CLK_APBCLK1_TRNGCKEN_Msk;
+        CLK->APBCLK0 |= CLK_APBCLK0_RTCCKEN_Msk;
 
-    TRNG->CTL = (0 << TRNG_CTL_CLKPSC_Pos);
+        RTC->LXTCTL |= (RTC_LXTCTL_C32KSEL_Msk | RTC_LXTCTL_LIRC32KEN_Msk); //To use LIRC32K
+
+        TRNG->ACT |= TRNG_ACT_ACT_Msk;
+        /* Waiting for ready */
+        i = 0;
+        while((TRNG->CTL & TRNG_CTL_READY_Msk) == 0)
+        {
+            if(i++ > timeout)
+            {
+                /* TRNG ready timeout */
+                return -1;
+            }
+        }
+
+        TRNG->CTL = (0 << TRNG_CTL_CLKPSC_Pos);
+    }
 
     u32Reg = TRNG->CTL;
     for(i = 0; i < (int32_t)len; i++)
     {
         TRNG->CTL = TRNG_CTL_TRNGEN_Msk | u32Reg;
         while((TRNG->CTL & TRNG_CTL_DVIF_Msk) == 0);
-        output[i] = TRNG->DATA;
+        *output++ = TRNG->DATA;
     }
 
     *olen = len;
 
-    return err;
+    return 0;
 }
